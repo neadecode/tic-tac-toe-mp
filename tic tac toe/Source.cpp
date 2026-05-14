45704603ea6576112daf26e2129f1c2d9cd38ec2
Jorge Andrés Jiménez <imnighty201@gmail.com>
2026-05-14 12:05:08 -0500

    we lack logic and proper selecting per player.

--- tic tac toe/Source.cpp
+++ tic tac toe/Source.cpp
@@ -17,30 +17,55 @@ struct AABB {
 
 };
 
+#define DARK_GRAY Color{128,128,128,255}
+#define HOVER_BTN Color{225,225,225,225}
+#define BG_BUTTON Color{192,192,192,255}
+#define OPAQUE    Color{255,255,255,105}
+
 struct Square {
 	CellState state = CellState::MT;
-	Vector2 position{};
+	Vector2 pos{};
 	AABB aabb{};
 	int size{};
 	bool selected = false;
+	Texture2D* xo = nullptr;
 	int px_s = 5;
+	Rectangle src{};
+	CellState plays = CellState::X;
 
-	void draw() const {
-		if (!selected) { drawNotSelected(); }
-		else {}
+	Rectangle getTexSrc(CellState s) const {
+		if (s == CellState::X) return { 0, 0, 32, 32 };
+		else if (s == CellState::O) return { 32, 0, 32, 32 };
+		return {};
+	}
+
+	void draw() {
+		auto [x, y] = pos;
+
+		src = getTexSrc(state);
+		
+		DrawRectangle(x + px_s, y + px_s, size - 2 * px_s, size - 2 * px_s, DARK_GRAY);
+		(!selected) ? drawNotSelected() : drawSelected();
+		drawPersistant();
 	}
 
 	void drawNotSelected() const {
-		auto [x, y] = position;
+		auto [x, y] = pos;
 		Vector2 mp = GetMousePosition();
 
-		DrawRectangle(x + px_s, y + px_s, size - 2 * px_s, size - 2 * px_s, { 128,128,128,255 });
-
 		if (aabb.isPointInside(mp)) {
-			DrawRectangle(x + px_s, y + px_s, size - 3 * px_s, size - 3 * px_s, { 225,225,225,225 });
+			DrawRectangle(x + px_s, y + px_s, size - 3 * px_s, size - 3 * px_s, HOVER_BTN);
+			DrawTexturePro(
+				*xo,
+				getTexSrc(plays),
+				{ x + px_s, y+ px_s, (float)size- 2*px_s, (float)size- 2*px_s },
+				{ 0, 0 }, 
+				0.f, 
+				OPAQUE
+			);
 		}
 		else {
-			DrawRectangle(x + px_s, y + px_s, size - 3 * px_s, size - 3 * px_s, { 192,192,192,255 });
+			DrawRectangle(x + px_s, y + px_s, size - 3 * px_s, size - 3 * px_s, BG_BUTTON);
 		}
 
 		DrawRectangle(x, y, size - px_s, px_s, WHITE);
@@ -49,13 +74,46 @@ struct Square {
 		DrawRectangle(size + x - px_s, y, px_s, size, BLACK);
 
 	}
+
+	void drawSelected() const {
+		auto [x, y] = pos;
+
+		DrawRectangle(x + 2*px_s, y + 2*px_s, size - 3 * px_s, size - 3 * px_s, BG_BUTTON);
+		DrawTexturePro(
+			*xo,
+			getTexSrc(plays),
+			{ x + 2* px_s, y + 2* px_s, (float)size - 3 * px_s, (float)size - 3 * px_s },
+			{ 0, 0 },
+			0.f,
+			OPAQUE
+		);
+
+		DrawRectangle(x, size + y - px_s, size, px_s, WHITE);
+		DrawRectangle(size + x - px_s, y, px_s, size, WHITE);
+		DrawRectangle(x, y, size, px_s, BLACK);
+		DrawRectangle(x, y, px_s, size, BLACK);
+	}
+
+	void drawPersistant() const {
+		auto [x, y] = pos;
+		DrawTexturePro(
+			*xo, src,
+			{ x + 2 * px_s, y + 2 * px_s, (float)size - 3 * px_s, (float)size - 3 * px_s },
+			{ 0, 0 },
+			0.f,
+			HOVER_BTN
+		);
+	}
 };
 
 template<typename S, int _width, int _height>
 struct GridManager {
+	static inline CellState currentPlayer = CellState::X;
+	static inline Texture2D xo{};
 	static inline int squareSize{};
 	static inline Vector2 padding{};
 	static const inline int gridSize = _width * _height;
+
 
 	static inline std::array<S, gridSize> shapes;
 	
@@ -84,11 +142,30 @@ struct GridManager {
 			Vector2 pos = { x, y };
 			AABB bb = { { x, y }, { x + sqrSize, y + sqrSize } };
 
-			shapes[i] = S{ CellState::MT, pos, bb, sqrSize, false};
+			shapes[i] = S{ CellState::MT, pos, bb, sqrSize, false, &xo};
+			shapes[i].plays = currentPlayer;
 		}
 	}
 
 	static void draw() { for (auto& shape : shapes) { shape.draw(); } }
+	
+	static void handleClick(Vector2 mp) {
+		for (auto& shape : shapes) {
+			if (shape.state == CellState::MT) {
+				shape.selected = (shape.aabb.isPointInside(mp) && IsMouseButtonDown(MOUSE_BUTTON_LEFT));
+			}
+			if (shape.aabb.isPointInside(mp) && IsMouseButtonReleased(MOUSE_BUTTON_LEFT) && shape.state == CellState::MT) {
+				shape.state = currentPlayer;
+				shape.selected = true;
+
+				currentPlayer = (currentPlayer == CellState::X) ? CellState::O : CellState::X;
+				shape.plays = currentPlayer;
+			}
+		}
+		
+	}
+private:
+	GridManager() = default;
 };
 
 
@@ -96,13 +173,17 @@ int main(void) {
 	InitWindow(1000, 1000, "Tic Tac Toe");
 	SetTargetFPS(40);
 
-	using Grid = GridManager<Square, 3, 3>;
+	using Grid = GridManager<Square, 3, 3>;	
+	Grid::xo = LoadTexture("this.png");
 	Grid::setup(200, {25.0f, 25.0f});
 
 	for (;;) {
 		if (WindowShouldClose()) break;
 		BeginDrawing();
 		ClearBackground(SKYBLUE);
+
+		Vector2 mp = GetMousePosition();
+		Grid::handleClick(mp);
 		Grid::draw();
 		EndDrawing();
 	}
